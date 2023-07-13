e08a1664cfe93b1f86763f1ab9119ef843d2498a
Pavel Poryvai <42907367+Pavel377dq@users.noreply.github.com>
2023-07-13 03:36:22 +0300

    +

--- src/index.js
+++ src/index.js
@@ -22,12 +22,12 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    // <React.StrictMode>
+     <React.StrictMode>
     <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
     </Provider>
-    // </React.StrictMode>
+     </React.StrictMode>
 );
 
